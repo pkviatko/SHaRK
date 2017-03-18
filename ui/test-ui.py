2a0ba6e1a008c26dcb305120517bbf1cbbb07398
pkviatko <pavelkvetko@gmail.com>
2017-03-18 23:03:51 +0300

    Create test MainWindow

---
+++ ui/test-ui.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtGui, QtCore, QtWidgets, uic
+import sys
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        uic.loadUi("NewSHaRK.ui", self)
+
+app = QtWidgets.QApplication(sys.argv)
+QtWidgets.QApplication.setStyle('fusion')
+main = MainWindow()
+main.show()
+sys.exit(app.exec_())
